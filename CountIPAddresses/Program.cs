54392118c2df2b54878c75d34286f73d021ecfa1
sergik.m <sergik.m@outlook.com>
2022-04-25 15:02:18 +0400

    Добавьте файлы проекта.

---
+++ CountIPAddresses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CountIPAddresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        using System;
+public class CountIPAddresses
+    {
+        public static long IpsBetween(string start, string end)
+        {
+            return -1;
+        }
+    }
+}
+}
