49dd8a36d49c3933e0d87086f6e9e0e6d7085ed6
sergik.m <sergik.m@outlook.com>
2022-04-25 16:35:07 +0400

    Добавлено описание и ссылка на задачу

--- CountIPAddresses/Program.cs
+++ CountIPAddresses/Program.cs
@@ -3,6 +3,14 @@ using System.Net;
 
 namespace CountIPAddresses
 {
+    // Count IP Addresses
+    //
+    // https://www.codewars.com/kata/526989a41034285187000de4/train/csharp
+    //
+
+    // Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+    //
+    // All inputs will be valid IPv4 addresses in the form of strings.The last address will always be greater than the first one.
     class Program
     {
         static void Main(string[] args)
